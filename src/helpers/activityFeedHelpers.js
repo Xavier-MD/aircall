1ffeefb99091d410460bce5c03d6744ea1503a54
Xavier Martin Demers <xavier.pmd@outlook.com>
2022-01-27 06:11:11 -0500

    Created helpers for the activity feed component.

---
+++ src/helpers/activityFeedHelpers.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import moment from 'moment';
+import { VoicemailRounded, PhoneCallbackRounded, PhoneForwardedRounded, PhoneMissedRounded, FeedRounded } from '@mui/icons-material';
+
+export function dateFormatter(date, format) {
+  const formattedDate = new Date(date);
+  if (format === 'date') {
+    const newDate = moment(formattedDate).format("MMMM Do YYYY")
+    console.log(newDate)
+    return newDate;
+  } else if (format === 'time') {
+    const newTime = moment(formattedDate).format('LT')
+    console.log(newTime)
+    return newTime;
+  }
+}
+
+export function callIcon(callDirection, callType) {
+  if (callType === 'voicemail') {
+    return <VoicemailRounded />
+  } else if (callType === 'answered' && callDirection === 'inbound') {
+    return (
+      <PhoneCallbackRounded />
+    );
+  } else if (callType === 'answered' && callDirection === 'outbound') {
+    return (
+      <PhoneForwardedRounded />
+    );
+  } else if (callType === 'missed') {
+    return (
+      <PhoneMissedRounded />
+    );
+  }
+}
+
+export function voicemailCheck(callType, toName) {
+  if (callType === 'voicemail') {
+    return 'Voicemail';
+  } else {
+    return toName;
+  }
+}
+
