08917443d9a71857e36af2b7c07fdcd688aced87
Xavier Martin Demers <xavier.pmd@outlook.com>
2022-01-27 06:12:22 -0500

    Added the activity feed to the app along with its required props.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header/Header.jsx';
+import ActivityFeed from './ActivityFeed/ActivityFeed.jsx';
+import useAppData from '../hooks/useAppData.js';
 
 const App = () => {
+  const { state, setCallId } = useAppData();
+
   return (
     <div className='container'>
       <Header/>
+      <ActivityFeed
+        calls={state.calls}
+        callId={state.callId}
+        setCallId={setCallId}
+        call={state.call}
+      />
     </div>
   );
 };
